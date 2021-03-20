af061143f3dd81a6dfa352981ea56a24397aa43e
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2021-03-20 00:38:25 -0500

    add Material UI select to pick format of color to be copied

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,24 @@
 import React, {Component} from "react";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {format: "hex"};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({format: event.target.value});
+        this.props.handleChange(event.target.value);
+    }
+
     render() {
+        const {format} = this.state;
         const {level, changeLevel} = this.props;
         return (
             <header className="Navbar">
@@ -17,7 +31,13 @@ class Navbar extends Component {
                         <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={changeLevel} />
                     </div>
                 </div>
-                
+                <div className="select-container">
+                    <Select value={format} onChange={this.handleChange}>
+                        <MenuItem value="hex">HEX - #ffffff</MenuItem>
+                        <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+                        <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
+                    </Select>
+                </div>
             </header>
         );
     }
