a213d15601428df3308c77e3a250b826e119de1c
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2021-03-24 18:13:23 -0500

    able to display colors in SingleColorPalette

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <Switch>
       <Route exact path="/" render={routeProps => <PaletteList {...routeProps} palettes={seedColors}/>}/>
       <Route exact path="/palette/:id" render={(routeProps) => <Palette palette={generatePalette(findPalette(routeProps.match.params.id))}/>} />
-      <Route path="/palette/:paletteId/:colorId" render={() => <SingleColorPalette />} />
+      <Route path="/palette/:paletteId/:colorId" render={(routeProps) => <SingleColorPalette colorId={routeProps.match.params.colorId} palette={generatePalette(findPalette(routeProps.match.params.paletteId))} />} />
     </Switch>
   );
 }
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,10 +1,43 @@
 import React, {Component} from "react";
+import ColorBox from "./ColorBox";
 
 class SingleColorPalette extends Component {
+    constructor(props) {
+        super(props);
+        this._shades = this.gatherShades();
+        this.state = {
+            copying: false
+        };
+        this.setCopying = this.setCopying.bind(this);
+    }
+
+    gatherShades() {
+        //return all shades of correct color
+        const {palette, colorId} = this.props;
+        const shades = [];
+        const allColors = palette.colors;
+        for (let key in allColors) {
+            shades.push(allColors[key].filter(color => color.id === colorId));
+        }
+        return shades.slice(1);
+    }
+
+    setCopying(value) {
+        this.setState({copying: value});
+    }
+
     render() {
+        const colorBoxes = this._shades.map(chosenColor => {
+            const color = chosenColor[0];
+            return <ColorBox key={color.id} name={color.name} background={color.hex} showLink={false} setCopying={this.setCopying} /> 
+        });
+
         return (
-            <div>
+            <div className="Palette">
                 <h1>Single color palette!</h1>
+                <div className="Palette-colors">
+                    {colorBoxes}
+                </div>
             </div>
         );
     }
