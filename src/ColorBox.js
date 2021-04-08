17bfa51d39f941346e1c719fc4976aa5719a3f33
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2021-04-08 16:00:23 -0500

    finish refactoring ColorBox css

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -58,6 +58,67 @@ const styles = {
         cursor: "pointer",
         textDecoration: "none",
         opacity: 0
+    },
+    boxContent: {
+        position: "absolute",
+        width: "calc(100%-20px)",
+        left: "0px",
+        bottom: "0px",
+        padding: "10px",
+        color: "black",
+        letterSpacing: "1px",
+        textTransform: "uppercase",
+        fontSize: "12px"
+    },
+    copyOverlay: {
+        opacity: "0",
+        zIndex: "0",
+        width: "100%",
+        height: "100%",
+        transition: "transform 0.6s ease-in-out",
+        transform: "scale(0.1)"
+    },
+    showOverlay: {
+        opacity: "1",
+        transform: "scale(50)",
+        zIndex: "10",
+        position: "absolute"
+    },
+    copyMessage: {
+        position: "fixed",
+        left: "0px",
+        right: "0px",
+        top: "0px",
+        bottom: "0px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "4rem",
+        transform: "scale(0.1)",
+        opacity: 0,
+        color: "white",
+        flexDirection: "column",
+        "& h1": {
+            fontWeight: 400,
+            textShadow: "1px 2px black",
+            background: "rgba(255,255,255,0.2)",
+            width: "100%",
+            textAlign: "center",
+            marginBottom: "0px",
+            padding: "1rem",
+            textTransform: "uppercase"
+        },
+        "& p": {
+            fontSize: "2rem",
+            fontWeight: 100
+        }
+    },
+    showCopyMessage: {
+        opacity: 1,
+        transform: "scale(1)",
+        zIndex: 25,
+        transition: "all 0.4s ease-in-out",
+        transitionDelay: "0.3s"
     }
 };
 
@@ -84,13 +145,13 @@ class ColorBox extends Component {
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{background}} className={classes.ColorBox}>
-                    <div style={{background}} className={`copy-overlay ${copied && "show"}`} />
-                    <div className={`copy-msg ${copied && "show"}`}>
+                    <div style={{background}} className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} />
+                    <div className={`${classes.copyMessage} ${copied && classes.showCopyMessage}`}>
                         <h1>copied!</h1>
                         <p className={classes.copyText}>{background}</p>
                     </div>
-                    <div className="copy-container">
-                        <div className="box-content">
+                    <div>
+                        <div className={classes.boxContent}>
                             <span className={classes.colorName}>{name}</span>
                         </div>
                         <button className={classes.copyButton}>Copy</button>
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,8 +1,42 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
+import {withStyles} from "@material-ui/styles";
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 import PaletteFooter from "./PaletteFooter";
+
+const styles = {
+    ColorBox: {
+        width: "20%",
+        height: "50%",
+        margin: "0 auto",
+        display: "inline-block",
+        position: "relative",
+        cursor: "pointer",
+        marginBottom: "-4px",
+        backgroundColor: "black"
+    },
+    backButton: {
+        width: "100px",
+        height: "30px",
+        position: "absolute",
+        display: "inline-block",
+        top: "50%",
+        left: "50%",
+        marginLeft: "-50px",
+        marginTop: "-15px",
+        textAlign: "center",
+        outline: "none",
+        background: "rgba(255,255,255,0.3)",
+        fontSize: "1rem",
+        lineHeight: "30px",
+        color: "white",
+        textTransform: "uppercase",
+        border: "none",
+        cursor: "pointer",
+        textDecoration: "none"
+    }
+}
 
 class SingleColorPalette extends Component {
     constructor(props) {
@@ -37,6 +71,7 @@ class SingleColorPalette extends Component {
 
     render() {
         const {id, paletteName, emoji} = this.props.palette;
+        const {classes} = this.props;
         const {format} = this.state;
         const colorBoxes = this._shades.map(chosenColor => {
             const color = chosenColor[0];
@@ -48,9 +83,9 @@ class SingleColorPalette extends Component {
                 <Navbar handleChange={this.changeFormat} showingAllColors={false} />
                 <div className="Palette-colors">
                     {colorBoxes}
-                    <div className="go-back ColorBox">
+                    <div className={classes.ColorBox}>
                     {/* <div className="go-back"> */}
-                        <Link className="back-button" to={`/palette/${id}`}>Go Back</Link>
+                        <Link className={classes.backButton} to={`/palette/${id}`}>Go Back</Link>
                     </div>
                 </div>
                 <PaletteFooter paletteName={paletteName} emoji={emoji} />
@@ -59,4 +94,4 @@ class SingleColorPalette extends Component {
     }
 }
 
-export default SingleColorPalette;
+export default withStyles(styles)(SingleColorPalette);
